a2302dcacc6c9a4f80ac0b22366f6df95c2ec4de
Zkaron <tibiapk95@hotmail.com>
2016-09-03 12:46:39 -0500

    some styles corrected

--- site/js/script.js
+++ site/js/script.js
@@ -13,19 +13,39 @@ function initMap() {
             streetViewControlOptions: {
                 position: google.maps.ControlPosition.LEFT_TOP
             },
+            mapTypeControl: false,
             scrollwheel: false,
             //draggable: !("ontouchend" in document),
+        });
+
+        var contentString = '<div id="content">' +
+        '<a href="https://www.google.com.mx/maps/place/20%C2%B042\'20.0%22N+103%C2%B023\'28.9%22W/@20.705551,-103.3935607,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d20.705551!4d-103.391372?hl=es-419">' +
+        'Ver en Google Maps</a>' +
+        '<h1>TaiChi</h1>' +
+        '<p>Informacion interesante del mapa, agregar capturas del lugar sera genial</p>' +
+        '</div>';
+
+        var infoWindow = new google.maps.InfoWindow({
+            content: contentString
         });
 
         var marker = new google.maps.Marker({
             position: myLatLng,
             map: map,
             title: 'TaiChi Colomos',
-            url: "https://www.google.com.mx/maps/place/20%C2%B042'20.0%22N+103%C2%B023'28.9%22W/@20.705551,-103.3935607,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d20.705551!4d-103.391372?hl=es-419"
         });
 
-        google.maps.event.addListener(marker, 'click', function() {
-            window.open(marker.url, 'blank');
+        marker.addListener('click', function() {  
+            infoWindow.open(map, marker);
         });
-
 }
+
+$(function () {     //same as document.addEventListener("DOMContentLoaded"...
+    //Same as document.querySelector("#navbarToggle").addEventListener("blur"),...
+    $("#navbarToggle").blur(function (event) {
+        var screenWidth = window.innerWidth;
+        if(screenWidth < 768) {
+            $("#collapsable-nav").collapse('hide');
+        }
+    });
+});
